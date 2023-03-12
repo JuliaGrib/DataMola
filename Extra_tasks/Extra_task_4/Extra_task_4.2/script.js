9e33f55fa52751e0ba74297d994bebcc58fddb9d
Julia Grib <yulgrib@gmail.com>
2023-03-12 13:25:19 +0300

    fix: add try catch, const value

--- Extra_tasks/Extra_task_4/Extra_task_4.2/script.js
+++ Extra_tasks/Extra_task_4/Extra_task_4.2/script.js
@@ -1,20 +1,35 @@
 "use strict";
 
+const LENGTH_VAL = {
+  minArr: 1,
+  maxArr: 30000,
+  minElem: 0,
+  maxElem: 10000,
+};
+
 const maxProfit = (arr) => {
-  if (arr.length < 1 || arr.length >= 3 * 10 ** 4) {
-    return `Длина массива должна быть больше 1 и меньше 30 000. Сейчас длина массива составляет ${arr.length}`;
-  }
-
-  let res = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < 0 || arr[i] >= 10 ** 4) {
-      return `Элемент массива может быть больше нуля и меньше 10 000. Элемент массива равен ${arr[i]}`;
+  try {
+    if (arr.length <= LENGTH_VAL.minArr || arr.length >= LENGTH_VAL.maxArr) {
+      throw new Error(
+        `Длина массива должна быть больше 1 и меньше 30 000. Сейчас длина массива составляет ${arr.length}`
+      );
     }
-    if (arr[i] < arr[i + 1]) {
-      res += arr[i + 1] - arr[i];
-    }
-  }
 
-  return res;
+    let res = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] <= LENGTH_VAL.minElem || arr[i] >= LENGTH_VAL.maxElem) {
+        throw new Error(
+          `Элемент массива может быть больше нуля и меньше 10 000. Элемент массива равен ${arr[i]}`
+        );
+      }
+      if (arr[i] < arr[i + 1]) {
+        res += arr[i + 1] - arr[i];
+      }
+    }
+
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
 };
