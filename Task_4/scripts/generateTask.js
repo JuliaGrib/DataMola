30218ef7964ed4f0fdb6d5eb66da0a7e0a1fb60a
Julia Grib <yulgrib@gmail.com>
2023-03-12 09:53:44 +0300

    feat: add card tools

--- Task_4/scripts/generateTask.js
+++ Task_4/scripts/generateTask.js
@@ -8,16 +8,29 @@ function makeCard(obj, parent) {
   const card = document.createElement("article");
   const cardHeader = document.createElement("div");
   const priority = document.createElement("span");
+  const cardTools = document.createElement("div");
+  const cardIconChange = document.createElement("a");
+  const cardIconDel = document.createElement("a");
 
   priority.innerHTML = obj.priority;
+  cardIconChange.innerHTML = ICONS.icon_change;
+  cardIconDel.innerHTML = ICONS.icon_del;
+  cardIconChange.href = LINKS.empty;
+  cardIconDel.href = LINKS.empty;
 
   card.classList.add("card");
   cardHeader.classList.add("card__header");
   priority.classList.add("priority", `priority_${obj.priority.toLowerCase()}`);
+  cardTools.classList.add("card__tools");
+  cardIconChange.classList.add("icon", "icon_change");
+  cardIconDel.classList.add("icon", "icon_del");
 
   parent.appendChild(card);
   card.appendChild(cardHeader);
   cardHeader.appendChild(priority);
+  cardHeader.appendChild(cardTools);
+  cardTools.appendChild(cardIconChange);
+  cardTools.appendChild(cardIconDel);
 }
 
 cols.forEach((elem) => {
