290ea7450e315582b92907ed05011de2f60661f1
Julia Grib <yulgrib@gmail.com>
2023-03-12 12:38:57 +0300

    fix: add try catch

--- Extra_tasks/Extra_task_4/Extra_task_4.1/script.js
+++ Extra_tasks/Extra_task_4/Extra_task_4.1/script.js
@@ -1,17 +1,24 @@
 "use strict";
 
 const findMaxSubArray = (arr) => {
-  if (!Array.isArray(arr)) {
-    return "Переданное значение не массив";
+  try {
+    if (!Array.isArray(arr)) {
+      throw new Error("Переданное значение не массив");
+    }
+    if (arr.length === 0) {
+      throw new Error("Массив не может быть пустым");
+    }
+
+    let current = 0;
+    let result = 0;
+
+    arr.forEach((elem) => {
+      current = Math.max(0, current + elem);
+      result = Math.max(result, current);
+    });
+
+    return result;
+  } catch (error) {
+    console.error(error);
   }
-
-  let current = 0;
-  let result = 0;
-
-  arr.forEach((elem) => {
-    current = Math.max(0, current + elem);
-    result = Math.max(result, current);
-  });
-
-  return result;
 };
