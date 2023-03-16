33072a86a33b7871f3bb36b45248b72c23e9e462
Julia Grib <yulgrib@gmail.com>
2023-03-16 12:57:27 +0300

    feat: add first four func

---
+++ Extra_tasks/Extra_task_5/Extra_task_5.2/script.js
@@ -0,0 +1,58 @@
+'use sctict';
+
+const ERROR = {
+  lengthNotValidate:
+    'Ожидается 1 или 2 аргумента, как написано в задаче, так что не надо пихать больше/меньше!',
+};
+
+function add(a, b) {
+  try {
+    return arguments.length === 2
+      ? a + b
+      : arguments.length === 1
+      ? (val) => val + a
+      : new Error(ERROR.lengthNotValidate);
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+function sub(a, b) {
+  try {
+    return arguments.length === 2
+      ? a - b
+      : arguments.length === 1
+      ? (val) => val - a
+      : new Error(ERROR.lengthNotValidate);
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+function mul(a, b) {
+  try {
+    return arguments.length === 2
+      ? a * b
+      : arguments.length === 1
+      ? (val) => val * a
+      : new Error(ERROR.lengthNotValidate);
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+function div(a, b) {
+  try {
+    return arguments.length === 2
+      ? a / b
+      : arguments.length === 1
+      ? (val) => val / a
+      : new Error(ERROR.lengthNotValidate);
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
