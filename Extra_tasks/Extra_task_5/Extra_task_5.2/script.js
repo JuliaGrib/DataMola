ae379468f4f0e3028ce0732462fc92effcb5e37e
Julia Grib <yulgrib@gmail.com>
2023-03-16 16:57:49 +0300

    feat: add doSmth and console.log

--- Extra_tasks/Extra_task_5/Extra_task_5.2/script.js
+++ Extra_tasks/Extra_task_5/Extra_task_5.2/script.js
@@ -65,3 +65,23 @@ function pipe(...arg) {
     return val;
   };
 }
+
+let a = add(1, 2);
+console.log(a);
+
+let b = mul(a, 10);
+console.log(b);
+
+let sub1 = sub(1);
+let c = sub1(b);
+console.log(c);
+
+let d = mul(sub(a, 1))(c);
+console.log(d);
+
+let doSmth = pipe(add(d), sub(c), mul(b), div(a));
+let result = doSmth(0);
+console.log(result);
+
+let x = pipe(add(1), mul(2))(3);
+console.log(x);
