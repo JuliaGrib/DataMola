22697e80dd32329c92474e728bf1d4b0bbc01978
Julia Grib <yulgrib@gmail.com>
2023-03-24 15:01:44 +0300

    fix: setCurrentUser

--- Task_6/app/app.js
+++ Task_6/app/app.js
@@ -2,7 +2,7 @@ const tasks = new TaskCollection(tasksDB);
 const headerView = new HeaderView('header__nav');
 
 function setCurrentUser(user) {
-  console.log((tasks.user = user));
+  tasks.user = user;
   headerView.display(tasks.user);
 }
 
