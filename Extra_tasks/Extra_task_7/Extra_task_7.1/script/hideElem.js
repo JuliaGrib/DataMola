87b7234dd871111cdbac883d694dcc6667e05b2c
Julia Grib <yulgrib@gmail.com>
2023-03-31 22:27:56 +0300

    fix: remove hideElem

--- Extra_tasks/Extra_task_7/Extra_task_7.1/script/hideElem.js
+++
@@ -1,8 +0,0 @@
-const hideElem = (event) => {
-  const elem = event.target;
-  const sibling = elem.nextSibling;
-
-  if (sibling?.tagName === 'UL') {
-    sibling.classList.toggle('hide');
-  }
-};
