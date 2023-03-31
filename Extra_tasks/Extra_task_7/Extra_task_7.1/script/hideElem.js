b87793684e8aafbee19ac06457df4018f63c1984
Julia Grib <yulgrib@gmail.com>
2023-03-31 22:10:05 +0300

    fix: remove console.log

--- Extra_tasks/Extra_task_7/Extra_task_7.1/script/hideElem.js
+++ Extra_tasks/Extra_task_7/Extra_task_7.1/script/hideElem.js
@@ -2,7 +2,6 @@ const hideElem = (event) => {
   const elem = event.target;
   const sibling = elem.nextSibling;
 
-  console.log(sibling?.tagName);
   if (sibling?.tagName === 'UL') {
     sibling.classList.toggle('hide');
   }
